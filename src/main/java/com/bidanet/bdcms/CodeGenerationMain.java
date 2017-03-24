ac25570d98ba7ad7f9dda364a9ef09b5312fd93b
xuejike <xuejike2004@126.com>
2017-03-24 17:37:06 +0800

    自动生成

---
+++ src/main/java/com/bidanet/bdcms/CodeGenerationMain.java
@@ -0,0 +1,19 @@
+package com.bidanet.bdcms;
+
+import com.bidanet.bdcms.core.codeGeneration.CodeGeneration;
+
+import com.bidanet.bdcms.entity.User;
+
+/**
+ * Created by xuejike on 2017/3/11.
+ */
+public class CodeGenerationMain {
+
+    public static void main(String[] args){
+        String daoPath="";
+        String daoImplPath="";
+        String servicePath="";
+        String serviceImplPath="";
+//        CodeGeneration.build(Advice.class,daoPath,daoImplPath,servicePath,serviceImplPath);
+         }
+}
